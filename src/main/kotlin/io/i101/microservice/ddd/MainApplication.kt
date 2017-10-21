b065f990605d38902a18fff6c043ab86ee59a44b
Thanh Dang <thanhtrdang@gmail.com>
2017-10-22 01:49:51 +0700

    Inactivate ComponentScan, if RepositorySupport init bug, then reactivate

--- src/main/kotlin/io/i101/microservice/ddd/MainApplication.kt
+++ src/main/kotlin/io/i101/microservice/ddd/MainApplication.kt
@@ -3,12 +3,11 @@ package io.i101.microservice.ddd
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
-@ComponentScan("io.i101.microservice.ddd", lazyInit = true)
+//@ComponentScan("io.i101.microservice.ddd", lazyInit = true)
 @EnableJpaRepositories
 @EnableTransactionManagement
 class MainApplication(context: ApplicationContext) {
